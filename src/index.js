2527f7bea59391c6d4d2a80ba99e70d4d0b0ee32
Roman <mazaxaka.tyt@gmail.com>
2022-01-31 20:22:23 +0200

    add header and text components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+import react from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const el = <h1>Hello React.js</h1>
+const Header = () => {
+    return <h1>Hello App component</h1>
+}
+const Text = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Aspernatur ullam nemo, temporibus earum exercitationem vitae
+                esse, dolore, asperiores sapiente excepturi maiores quae. Vel
+                quas perspiciatis hic nesciunt distinctio neque dolore!
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Aspernatur ullam nemo, temporibus earum exercitationem vitae
+                esse, dolore, asperiores sapiente excepturi maiores quae. Vel
+                quas perspiciatis hic nesciunt distinctio neque dolore!
+            </p>
+        </>
+    )
+}
 
-ReactDOM.render(el, document.getElementById('root'))
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Text />
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

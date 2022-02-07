6896b5b42051bade258021a8b758a7debe4ab1d2
Roman <mazaxaka.tyt@gmail.com>
2022-02-07 21:04:52 +0200

    add productListItem from productArray

---
+++ src/components/Products/productsArray.js
@@ -0,0 +1,44 @@
+export const productsArray = [
+    {
+        name: 'iPhone 13 Pro',
+        description: 'This is iPhone 13 Pro',
+        capacity: '264',
+        type: 'phone',
+        price: '1100',
+    },
+    {
+        name: 'iPhone 12 Pro',
+        description: 'This is iPhone 12 Pro',
+        capacity: '64',
+        type: 'phone',
+        price: '800',
+    },
+    {
+        name: 'iPhone X',
+        description: 'This is iPhone X',
+        capacity: '64',
+        type: 'phone',
+        price: '500',
+    },
+    {
+        name: 'iPhone 13 Pro Max',
+        description: 'This is 13 Pro Max',
+        capacity: '512',
+        type: 'phone',
+        price: '2500',
+    },
+    {
+        name: 'iPhone X',
+        description: 'This is iPhone X',
+        capacity: '64',
+        type: 'phone',
+        price: '500',
+    },
+    {
+        name: 'iPhone 12 Pro',
+        description: 'This is iPhone 12 Pro',
+        capacity: '64',
+        type: 'phone',
+        price: '800',
+    },
+]
